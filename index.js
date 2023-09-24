606c3680213b8b921ef54f1dcdad223b57bdefd1
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-09-24 13:43:16 +0300

    Main page test

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const hbs = require("express-handlebars").create({
   extname: ".hbs",
 });
+const homeController = require("./controllers/homeController");
 
 const app = express();
 
@@ -9,7 +10,8 @@ app.engine(".hbs", hbs.engine);
 app.set("view engine", ".hbs");
 
 app.use(express.urlencoded({ extended: true }));
-
 app.use("/", express.static("static"));
+
+app.use(homeController);
 
 app.listen(3000, () => console.log("Server is working on port 3000"));
