9cb448614ec730f7f612597a6dfb796a46e6c13a
Emin A <122976155+eminn-a@users.noreply.github.com>
2023-09-24 17:15:22 +0300

    Catalog and Details

--- controllers/catalogController.js
+++ controllers/catalogController.js
@@ -1,15 +1,31 @@
+const { getAll, getById } = require("../services/accoService");
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
+  const rooms = getAll;
+  console.log(rooms);
   res.render("catalog", {
     title: "Catalog",
+    rooms,
   });
 });
 
 router.get("/:id", (req, res) => {
-  res.render("details", {
-    title: "Details",
-  });
+  const roomId = req.params.id;
+  const room = getById(roomId);
+  console.log(room);
+
+  if (room) {
+    res.render("details", {
+      title: "Details",
+      room,
+    });
+  } else {
+    res.render("roomNotFound", {
+      title: "Not Found!",
+      roomId,
+    });
+  }
 });
 
 module.exports = router;
